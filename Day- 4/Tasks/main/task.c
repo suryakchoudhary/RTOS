6a625c19a856c1fe0bbca3ebba82c77e37cee3b6
suryakchoudhary <surya333kant@gmail.com>
2022-01-20 16:30:57 +0530

    Creating Multiple Tasks

---
+++ Day- 4/Tasks/main/task.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void task_1(void *tsk)
+{
+	while(1)
+	{
+		printf("TASK 1 IN PROCESS\n");
+	}
+	vTaskDelete(NULL);
+}
+
+void task_2(void *tsk)
+{
+	while(1)
+	{
+		printf("TASK 2 IN PROCESS\n");
+	}
+	vTaskDelete(NULL);
+}
+
+void app_main()
+{
+	BaseType_t result;
+
+	result = xTaskCreate(task_1, "Process_1", 1024, NULL, 5, NULL);
+	if(result != pdPASS)
+	{
+		printf("Task 1 NOT CREATED\n");
+	}
+
+
+	result = xTaskCreate(task_2, "Process_2", 1024, NULL, 4, NULL);
+	if(result != pdPASS)
+	{
+		printf("Task 2 NOT CREATED\n");
+	}
+}
